e4db4a24d1dcee9d9428277f3d30339aea88a46e
Taggagii <jdietrich0603@gmail.com>
2020-10-12 12:52:33 -0400

    tic tac toe has been added

--- bot.js
+++ bot.js
@@ -48,9 +48,61 @@ const commands = { //command name, passable arguments, discription, minimum perm
     "tic" : {
         ARGS : "",
         DISCRIPTION : "plays tick tac toe",
-        MINPERMISSIONS : 4
+        MINPERMISSIONS : 2
     }
 }
+
+//tictactoe data
+var games = {};
+var boardMessage = '';
+
+async function draw_board(gameData, message)
+{
+    let outputBoard = `${gameData.slice(0, 3).join('   ')}\n${gameData.slice(3, 6).join('   ')}\n${gameData.slice(6, 9).join('   ')}`;
+    var boardMessage = await message.channel.send(outputBoard);
+}
+
+function check_win(gameValues, symbol, message)
+{
+    let gameData = gameValues[0];
+    for (var start = 0; start < 3; start++)//checking straights
+    {
+        if (gameData[start] === symbol && gameData[start + 3] === symbol && gameData[start + 6] === symbol) 
+        {
+            message.channel.send(`${symbol} has won with a vertical line starting at ${start}`);
+            return true;
+        }
+        
+        if (gameData[start * 3] === symbol && gameData[start * 3 + 1] === symbol && gameData[start * 3 + 2] === symbol)
+        {
+            message.channel.send(`${symbol} has won with a horizontal line starting at ${start * 3}`);
+            return true;
+        }
+    }
+    if (gameData[0] === symbol && gameData[4] === symbol && gameData[8] === symbol) //checking two diagonals 
+    {
+        message.channel.send(`${symbol} has won with a diagonal starting at 0`);
+        return true;
+    }
+    if (gameData[2] === symbol && gameData[4] === symbol && gameData[6] === symbol)
+    {
+        message.channel.send(`${symbol} has won with a diagonal starting at 2`);
+        return true;
+    }
+    if (!gameData.includes('-')) //checking for full board
+    {
+        message.channel.send(`The game ends in a tie. Neither player wins`);
+        return true;
+    }
+    return false; //returning false is the game is not over yet
+
+}
+
+
+
+
+
+
 
 const commandList = Object.keys(commands);
 
@@ -117,17 +169,17 @@ client.on("message", async message => {
         .split(/\s+/);
         
         var membersPermissions = getPermissionsLevel(message);
-        let memberHasAccessTo = (command, message) => {
+        let memberHasAccessTo = (command) => {
             if (membersPermissions >= commands[command].MINPERMISSIONS)
                 return true;
-            message.channel.send(`You do not have high enough permissions to access ".${command}".\nType ".commands" to see commands you have permissions to access.`)
+                message.channel.send(`You do not have high enough permissions to access ".${command}".\nType ".commands" to see commands you have permissions to access.`)
             return false;
         }
         
         switch(command_name)
         {
             case commandList[0]: //commands
-                if (memberHasAccessTo(commandList[0], message))
+                if (memberHasAccessTo(commandList[0]))
                 {
                     var outputString = "";
                     Object.keys(commands).forEach(command => {
@@ -139,7 +191,7 @@ client.on("message", async message => {
                 break;
 
             case commandList[1]: //permissions
-                if (memberHasAccessTo(commandList[1], message))
+                if (memberHasAccessTo(commandList[1]))
                 {
                     if (!membersPermissions)
                         message.channel.send("You do not have a permisions level");
@@ -149,7 +201,7 @@ client.on("message", async message => {
                 break;
 
             case commandList[2]: //shutdown
-                if (memberHasAccessTo(commandList[2], message))
+                if (memberHasAccessTo(commandList[2]))
                 {
                     if (message.author.id === OwnerId)
                         message.channel.send("You are authorized. Shutting Down").then(m => {client.destroy();});
@@ -159,7 +211,7 @@ client.on("message", async message => {
                 break;
 
             case commandList[3]: //user
-                if (memberHasAccessTo(commandList[3], message))
+                if (memberHasAccessTo(commandList[3]))
                 {
                     var outputString = "";
                     if (args.length === 0)
@@ -178,7 +230,7 @@ client.on("message", async message => {
                 break;
 
             case commandList[4]: //kick
-                if (memberHasAccessTo(commandList[4], message))
+                if (memberHasAccessTo(commandList[4]))
                 {
                     if (args.length === 0) return message.reply("A user ID must be provided for this command");
                     var member = message.guild.member(message.mentions.users.first());
@@ -203,7 +255,7 @@ client.on("message", async message => {
                 break;
 
             case commandList[5]: //ban
-                if (memberHasAccessTo(commandList[5], message))
+                if (memberHasAccessTo(commandList[5]))
                 {
                     if (args.length === 0) return message.reply("A user ID must be provided for this command");
                     var member = message.guild.member(message.mentions.users.first());
@@ -229,7 +281,7 @@ client.on("message", async message => {
                 break;
             
             case commandList[6]: //display
-                if (memberHasAccessTo(commandList[6], message))
+                if (memberHasAccessTo(commandList[6]))
                 {
                     if (args.length === 0)
                     {
@@ -265,7 +317,7 @@ client.on("message", async message => {
                 break;
 
             case commandList[7]: //deletePastMessages
-                if (memberHasAccessTo(commandList[7], message) && 1 === 2)
+                if (memberHasAccessTo(commandList[7]) && 1 === 2)
                 {
                     var deleteBackDistance = 1;
                     if (args.length != 0 && !isNaN(args[0]) && args[0] > 0)
@@ -281,29 +333,119 @@ client.on("message", async message => {
             case commandList[8]: //tic
                 if (memberHasAccessTo(commandList[8]))
                 {
-                    let playerOne = message.member;
-                    if (args.length === 0)
+                    if (args.length != 0 && args[0] === "move")
+                    {
+                        let player = message.member.id;
+                        if (!games[player])
+                        {
+                            message.reply("You are not currently in a game, you can start one by typeing '.tic args = {@username}'");
+                            break;
+                        }
+                        if (player !== games[player][1][games[player][2]].id)
+                        {
+                            message.channel.send("it is not your turn");
+                            break;
+                        }
+                        let symbol = "x";
+                        if (games[player][2] === 1)
+                            symbol = "o";
+                        let moved = true;
+                        var location = '';
+                        switch (args[1])
+                        {
+                            case "tl":
+                                location = 0;
+                                break;
+                            case "tm":
+                                location = 1; 
+                                break;
+                            case "tr":
+                                location = 2; 
+                                break;
+                            case "ml":
+                                location = 3; 
+                                break;
+                            case "mm":
+                                location = 4; 
+                                break;
+                            case "mr":
+                                location = 5; 
+                                break;
+                            case "bl":
+                                location = 6; 
+                                break;
+                            case "bm":
+                                location = 7; 
+                                break;
+                            case "br":
+                                location = 8; 
+                                break;
+                            default:
+                                message.channel.send("invaild movement : vaild movements are as follows\n(tl, tm, tr, ml, mm, mr, bl, bm, br)");
+                                moved = false;
+                                break;
+                            }
+                        if (games[player][0][location] === '-')
+                        {
+                            games[player][0][location] = symbol;
+                        }
+                        else
+                        {
+                            moved = false;
+                            message.channel.send("the spot already has a piece in it, please choose a new location");
+                        }
+                        let gameOver = check_win(games[player], symbol, message);
+                        draw_board(games[player][0], message);
+                        if (!moved) break;
+                        if (gameOver)
+                        {
+                            let players = games[player][1];
+                            delete games[players[0].id];
+                            delete games[players[1].id];
+                            break;
+                        }
+
+                        let nextPlayer = (games[player][2] + 1) % 2;
+                        games[player][2] = nextPlayer;
+                        games[games[player][1][nextPlayer].id] = games[player];
+                        break;
+                    }
+                    if (args.includes("games"))
+                    {
+                        if (Object.keys(games).length === 0)
+                        {
+                            message.channel.send("There are currently no games running. Start a game by typing '.tic args = {@username}'");
+                        }
+                        for (let game in games)
+                        {
+                            let currentGame = games[game];
+                            message.channel.send(`${currentGame[1][0].displayName} is playing with ${currentGame[1][1].displayName}\n${currentGame[0].slice(0, 3).join('   ')}\n${currentGame[0].slice(3, 6).join('   ')}\n${currentGame[0].slice(6, 9).join('   ')}\nit is currently ${currentGame[1][currentGame[2]]}'s turn`);
+                        }
+                        break;
+                    }
+                    if (!message.mentions)
                     {
                         message.channel.send("You must '@' mention the other player to play tic-tac-toe");
-                        return;
+                        break;
                     }
-                    let playerTwo = message.mentions.members.first();
-                    message.channel.send(`${playerOne.displayName} is playing tic-tac-toe with ${message.mentions.members.first().displayName}`)
-                    message.channel.send(`user ids : ${playerOne.id} ${playerTwo.id}`);
-                    function draw_board(gameData, message)
+                    else
                     {
-                        var outputBoard = gameData.slice(0, 3).join('  ');
-                        outputBoard += "\n";
-                        outputBoard += gameData.slice(3, 6).join('  ');
-                        outputBoard += "\n";
-                        outputBoard += gameData.slice(6, 9).join('  ');
-                        message.channel.send(outputBoard);
+                        let players = [message.member, message.mentions.members.first()];
+                        message.channel.send(`${players[0].displayName} is playing tic-tac-toe with ${players[1].displayName}`)
+    
+                        let gameData = ['-', '-', '-', '-', '-', '-', '-', '-', '-'];
+                        draw_board(gameData, message);
+    
+                        let firstPlayer = Math.floor(Math.random() * 10) % 2;
+                        message.channel.send(`${players[firstPlayer].displayName} moves first`);                                                                            //FOR TESTING
+                        games[players[firstPlayer].id] = [gameData, players, firstPlayer];
+                        break;
                     }
-                    var gameData = ['-', '-', '-', '-', '-', '-', '-', '-', '-',];
-                    draw_board(gameData, message);
-
+                    
+                    
                 }
                 break;
+            
 
             case "..":
                 break;
